7a7439f6b636ad7cd83fceb6348e288f6df23058
Gary Sims <gary.sims@gmail.com>
2023-06-15 13:39:57 +0300

    Dev

--- upload2pb.c
+++ upload2pb.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+/*
+ * Compile with:
+ * gcc -o upload2pb upload2pb.c
+*/
+
 int main(int argc, char *argv[]) {
     FILE *fp;
     char ch;
+
+    if (argc != 3) {
+        printf("Usage: %s <filename> <device>\n");
+        printf("eg: %s main.bas /dev/ttyACM0\n");
+        return 1;
+    }
 
     fp = fopen(argv[1], "r");
 
