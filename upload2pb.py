4104e78d2aa1456caf2ac56fdb260e8a501bacae
Gary Sims <gary.sims@gmail.com>
2023-06-16 14:47:51 +0300

    dev work

--- upload2pb.py
+++ upload2pb.py
@@ -2,10 +2,19 @@
 import time
 import serial
 import sys
+import os
+
+print("PiccoloBASIC uploader.")
 
 if len(sys.argv)!=3:
     print("Usage: " + sys.argv[0] + "<filename> <device>");
     print("eg: " + sys.argv[0] + " main.bas /dev/ttyACM0");
+    exit(1)
+
+print("Uploading " + sys.argv[1] + " to piccoloBASIC via " + sys.argv[2])
+file_stats = os.stat(sys.argv[1])
+if file_stats.st_size < 0:
+    print("Can't get file size of " + sys.argv[1])
     exit(1)
 
 ser = serial.Serial(
@@ -33,15 +42,13 @@ while 1:
         if "+OK PiccoloBASIC CMD Mode" in rstr:
             break
 print("Entered CMD mode.")
-print("Uploading " + sys.argv[1] + " to piccoloBASIC via " + sys.argv[2])
-
-cmd = "ls\n"
+    
+cmd = "upload " + sys.argv[1] + " " + str(file_stats.st_size) + "\n"
 packet = bytearray(cmd, 'ascii')
 ser.write(packet)
 
 # Wait to enter CMD mode
 while 1:
-        x=ser.readline()
         r=ser.readline()
         rstr = str(r)
         if len(rstr) == 0:
